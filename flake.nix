5b48cc9931905bb97a90c60f18786e11734e8950
Niels Kristian Lyshøj Jensen <n@nk.je>
2021-04-19 02:15:34 +0200

    Make pkgs follow same nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,13 +1,20 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable"; 
+
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
 
     my-very-cool-background.url = "github:NKJe/nixscopy?dir=my-very-cool-background";
+    my-very-cool-background.inputs.nixpkgs.follows = "nixpkgs";
+
     sddm-sugar-candy.url = "github:NKJe/nixscopy?dir=sddm-sugar-candy";
+    sddm-sugar-candy.inputs.nixpkgs.follows = "nixpkgs";
+
     zsh-autocomplete.url = "github:NKJe/nixscopy?dir=zsh-autocomplete";
+    zsh-autocomplete.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, my-very-cool-background, sddm-sugar-candy, zsh-autocomplete, ... }:
